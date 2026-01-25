52db1c76cfaec9225bded057bef81dc2021c2bf0
dsemkin <semkin_dmitriy10@vk.com>
2026-01-25 22:38:09 +0400

    Initial commit

---
+++ src/main/java/ru/andef/andefracing/backend/AndefRacingBackendApplication.java
@@ -0,0 +1,11 @@
+package ru.andef.andefracing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndefRacingBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AndefRacingBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/andef/andefracing/backend/AndefRacingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andef.andefracing.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndefRacingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
