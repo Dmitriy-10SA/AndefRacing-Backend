3cd8bf958e266042c37bf4604b7da27372b67e66
dsemkin <semkin_dmitriy10@vk.com>
2026-03-03 10:10:52 +0400

    Настройка безопасности

--- src/main/java/ru/andef/andefracing/backend/network/security/SecurityConfig.java
+++ src/main/java/ru/andef/andefracing/backend/network/security/SecurityConfig.java
@@ -8,12 +8,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.andef.andefracing.backend.data.entities.club.hr.EmployeeRole;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
     private final JwtProperties jwtProperties;
+    private final String[] allEmployeeRoles = Arrays.stream(EmployeeRole.values())
+            .map(EmployeeRole::getRole)
+            .toArray(String[]::new);
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
@@ -21,6 +27,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(it -> {
                     it.requestMatchers("/auth/**").permitAll();
+                    it.requestMatchers("/client/**").hasRole(jwtProperties.getClientRole());
+                    it.requestMatchers("/employee/profile/**").hasAnyRole(allEmployeeRoles);
                 })
                 .sessionManagement(it ->
                         it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
