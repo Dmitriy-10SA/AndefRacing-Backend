2a9ed169055398b650600b10d549f9fc3f0bee9e
dsemkin <semkin_dmitriy10@vk.com>
2026-03-19 14:22:59 +0400

    Добавил комментарии для пагинации бронирований

--- src/main/java/ru/andef/andefracing/backend/network/dtos/booking/client/PagedClientBookingShortListDto.java
+++ src/main/java/ru/andef/andefracing/backend/network/dtos/booking/client/PagedClientBookingShortListDto.java
@@ -4,6 +4,9 @@ import ru.andef.andefracing.backend.network.dtos.common.PageInfoDto;
 
 import java.util.List;
 
+/**
+ * Список бронирований для клиента с пагинацией
+ */
 public record PagedClientBookingShortListDto(
         List<ClientBookingShortDto> content,
         PageInfoDto pageInfo
--- src/main/java/ru/andef/andefracing/backend/network/dtos/booking/employee/PagedEmployeeBookingShortListDto.java
+++ src/main/java/ru/andef/andefracing/backend/network/dtos/booking/employee/PagedEmployeeBookingShortListDto.java
@@ -4,6 +4,9 @@ import ru.andef.andefracing.backend.network.dtos.common.PageInfoDto;
 
 import java.util.List;
 
+/**
+ * Список бронирований для сотрудника с пагинацией
+ */
 public record PagedEmployeeBookingShortListDto(
         List<EmployeeBookingShortDto> content,
         PageInfoDto pageInfo
